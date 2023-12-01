5a68c2d74b5b84a68c06c2206405056166b283c0
Spencer Au <spencerau96@gmail.com>
2023-11-30 16:59:45 -0800

    cron job and ftp

--- doggo.sh
+++ doggo.sh
@@ -10,13 +10,15 @@ for process in $processes; do
   fi
 done
 
+clear
+
 # Open terminal window for FindMyHistory and run main.py
 osascript -e 'tell application "Terminal" to do script "cd /Users/spencerau/FindMyHistory && python3 main.py"'
 
 # Adding a short sleep to ensure main.py has started
-sleep 3
+#sleep 3
 
 # Navigate to the where-is-sandy directory and run doggo.py
-osascript -e 'tell application "Terminal" to do script "cd /Users/spencerau/Documents/GitHub/where-is-sandy && python3 doggo.py"'
+#osascript -e 'tell application "Terminal" to do script "cd /Users/spencerau/Documents/GitHub/where-is-sandy && python3 doggo.py"'
 
 osascript -e 'tell application "Terminal" to do script "cd /Users/spencerau/Documents/GitHub/where-is-sandy && node server.js"'
