2db0bbc97bbe7a8257e6b55c59c473ea5f464fee
Spencer Au <30947985+spencerau@users.noreply.github.com>
2024-09-23 17:13:48 -0700

    .

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # where-is-sandy
  
+## Requirements
+- Run `pip3 install -r requirements.txt` to install the required packages. 
--- doggo.sh
+++ doggo.sh
@@ -13,12 +13,7 @@ done
 clear
 
 # Open terminal window for FindMyHistory and run main.py
-osascript -e 'tell application "Terminal" to do script "cd /Users/spencerau/Documents/GitHub/where-is-sandy/FindMyHistory && python3 main.py"'
+osascript -e 'tell application "Terminal" to do script "cd /Users/spencerau/Documents/GitHub/where-is-sandie/FindMyHistory && python3 main.py"'
 
 # Adding a short sleep to ensure main.py has started
 #sleep 3
-
-# Navigate to the where-is-sandy directory and run doggo.py
-#osascript -e 'tell application "Terminal" to do script "cd /Users/spencerau/Documents/GitHub/where-is-sandy && python3 doggo.py"'
-
-#osascript -e 'tell application "Terminal" to do script "cd /Users/spencerau/Documents/GitHub/where-is-sandy && node server.js"'
