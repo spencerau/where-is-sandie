0ee761b4b4358e1646653a7e89d486c0a0b6dd5f
Spencer Au <30947985+spencerau@users.noreply.github.com>
2024-10-31 18:42:44 -0700

    update FindMyHistory fork

--- doggo.sh
+++ doggo.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Get list of open terminal processes
 processes=$(osascript -e 'tell application "System Events" to get the name of every process whose visible is true')
 
-# Loop through processes and close terminals
 for process in $processes; do
   if [[ $process == Terminal ]]; then
     osascript -e 'tell application "Terminal" to quit'
@@ -11,6 +9,8 @@ for process in $processes; do
 done
 
 clear
+
+rm -rf /Users/spencerau/Documents/GitHub/where-is-sandie/FindMyHistory/log/*
 
 # Open terminal window for FindMyHistory and run main.py
 osascript -e 'tell application "Terminal" to do script "cd /Users/spencerau/Documents/GitHub/where-is-sandie/FindMyHistory && python3 main.py"'
